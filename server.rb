7a287091987adce8cfbd92e4a2d8b8d484a5bf98
Maros Hluska <mhluska@gmail.com>
2013-12-02 21:59:06 -0600

    Added debug output.

--- server.rb
+++ server.rb
@@ -1,47 +1,55 @@
 require 'sinatra'
 require 'sinatra/config_file'
 require 'httparty'
+require 'logger'
 
-puts "#{Dir.pwd}/config.yml"
 config_file "#{Dir.pwd}/config.yml"
+LOGGER = Logger.new('debug.log', 'daily')
 
 class Game
+    include HTTParty
+    debug_output $stderr
+
     WALLET_GUID = 'da48e315-5ada-4e41-b5d8-0114e6033f1c'
     WALLET_ADDRESS = '1BZSrMWNETLSEs4bFEnxxz1UX4c2uPeudz'
-    WALLET_PASSWORD = ARGV[0]
+    WALLET_PASSWORD = ENV['WALLET_PASSWORD']
     PAYMENT_URL = "https://blockchain.info/merchant/#{WALLET_GUID}/payment"
     HOUSE_EDGE = 0.01
     SATOSHI_PER_BITCOIN = 100000000 
 
-    def init(payout_address)
+    def initialize(site_name, payout_address)
+        @site_name = site_name
         @payout_address = payout_address
     end
 
     # Amount is in Satoshi.
     def bet(amount)
         if win_bet
-            payout_amount = amount * (2 - HOUSE_EDGE)
-            note = self.make_note(amount, payout_amount)
-            self.payout(payout_amount, note)
+            payout_amount = (amount * (2 - HOUSE_EDGE)).round
+            note = make_note(amount, payout_amount)
+            payout(payout_amount, note)
+            LOGGER.debug(note)
+        else
+            LOGGER.debug('Lost bet')
         end
     end
     
     private
 
     def make_note(amount, payout_amount)
-        bet = amount / SATOSHI_PER_BITCOIN
-        winnings = payout_amount / SATOSHI_PER_BITCOIN
-        "#{settings.site_name}: Bet #{bet}, won #{winnings}"
+        bet = amount / SATOSHI_PER_BITCOIN.to_f
+        winnings = payout_amount / SATOSHI_PER_BITCOIN.to_f
+        "#{@site_name}: Bet #{bet}, won #{winnings}"
     end
 
     def payout(amount, note='')
-        HTTParty.post(PAYMENT_URL, 
-            main_password: WALLET_PASSWORD,
+        LOGGER.debug(self.class.post(PAYMENT_URL, query: {
+            password: WALLET_PASSWORD,
             to: @payout_address,
             amount: amount,
             from: WALLET_ADDRESS,
             note: note
-        )
+        }))
     end
 
     def win_bet
@@ -56,15 +64,15 @@ get '/' do
     }
 end
 
-post '/bet' do
+get '/bet' do
     if params[:test]
         return
     end
 
+    puts params
+
     # TODO: Verify secret.
 
-    # Bitcoin amount received from Blockchain API.
-    amount = 50
-    game = Game.new(payout_address: params[:input_address])
-    game.bet(params[:amount])
+    game = Game.new(settings.site_name, params[:input_address])
+    game.bet(params[:value].to_i)
 end
